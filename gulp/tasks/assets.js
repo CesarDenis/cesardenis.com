e1188369c0ab031d2140b26be1468b27405a49ef
Cesar Denis <cesar@cesardenis.com>
2017-08-21 08:27:05 -0300

    Added fullscreen hero section

--- gulp/tasks/assets.js
+++ gulp/tasks/assets.js
@@ -25,6 +25,8 @@ gulp.task('scripts', () =>
   // NOTE: The order here is important since it's concatenated in order from
   // top to bottom, so you want vendor scripts etc on top
   gulp.src([
+    'bower_components/jquery/dist/jquery.js',
+    'bower_components/jquery.easing/jquery.easing.js',
     'src/assets/javascript/vendor.js',
     'src/assets/javascript/main.js'
   ])
--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -1,1 +1,12 @@
 
+(function($) {
+  'use strict';
+  
+  $(".page-scroll a").on("click", function(event) {
+    var link = $(this);
+    $("html, body").stop().animate({
+      scrollTop: $(link.attr("href")).offset().top
+    }, 1500, "easeInOutExpo"), event.preventDefault()
+  });
+  
+}(jQuery));
