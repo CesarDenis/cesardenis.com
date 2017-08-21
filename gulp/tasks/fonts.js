3970827840eefd69b578f0137774f88b5a6473cd
Cesar Denis <cesar@cesardenis.com>
2017-08-21 07:19:31 -0300

    Added Font Awesome

--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -4,7 +4,8 @@ const size = require('gulp-size');
 
 // 'gulp fonts' -- copies your fonts to the temporary assets directory
 gulp.task('fonts', () =>
-  gulp.src('src/assets/fonts/**/*')
+  gulp.src(require('main-bower-files')('**/*.{eot,svg,ttf,woff,woff2}', function (err) {})
+    .concat('src/assets/fonts/**/*'))
     .pipe(gulp.dest('.tmp/assets/fonts'))
     .pipe(size({title: 'fonts'}))
 );
