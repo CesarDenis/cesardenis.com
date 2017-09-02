aa540bb77533794064e1931e68f8b7d6f90bd0fe
Cesar Denis <cesar@cesardenis.com>
2017-09-02 15:52:15 -0300

    Update navbar

--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -5,7 +5,7 @@
   $(".page-scroll a").on("click", function(event) {
     var link = $(this);
     $("html, body").stop().animate({
-      scrollTop: $(link.attr("href")).offset().top
+      scrollTop: $(link.attr("href")).offset().top - 59
     }, 1500, "easeInOutExpo"), event.preventDefault()
   });
 
